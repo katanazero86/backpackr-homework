03783e7996b255dca53f9ad788d16a1930a5a10a
ChangHyeon Bae <katanazero86@gmail.com>
2020-06-11 01:03:09 +0900

    [feature] 컴포넌트 작업 - README.md 내용 작성 및 코드 확인 - 유닛테스트 fail 부분 수정

--- tests/unit/components.spec.js
+++ tests/unit/components.spec.js
@@ -1,9 +1,12 @@
 import { createLocalVue, shallowMount } from '@vue/test-utils';
 import Vuex from 'vuex';
 import store from '@/store/index';
+import VueRouter from 'vue-router';
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
+localVue.use(VueRouter);
+const router = new VueRouter();
 
 import App from '@/App.vue';
 import Index from '@/views/Index.vue';
@@ -13,7 +16,7 @@ import CardHorizontalComponent from "@/components/card/CardHorizontalComponent";
 
 describe('component is a Vue instance', () => {
     test('App Component is a Vue instance', () => {
-        const wrapper = shallowMount(App);
+        const wrapper = shallowMount(App, {router, localVue});
         expect(wrapper).toBeTruthy();
     });
 
